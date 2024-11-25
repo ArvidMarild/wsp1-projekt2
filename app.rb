5c39b0a24095a3d62f1fd6dbc484ea7873d6ec5a
ArvidMarild <152278740+ArvidMarild@users.noreply.github.com>
2024-11-25 13:00:49 +0100

    firts commit :P

--- app.rb
+++ app.rb
@@ -4,4 +4,27 @@ class App < Sinatra::Base
         erb(:"index")
     end
 
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/todo.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
+    get '/task' do
+        @tasks = db.execute('SELECT * FROM todo')
+        @ongoing_tasks = db.execute('SELECT * FROM todo ')
+    end
+
+    post '/todo/' do
+        db.execute("INSERT INTO todo (title, beskrivning, subject) VALUES(?,?,?)", 
+        [   
+            params["title"],
+            params["beskrivning"],
+            params["subject"]
+        ])
+        redirect ""
+    end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,9 +3,33 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todo')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE todo (
+                title TEXT NOT NULL,
+                beskrivning TEXT,
+                subject TEXT)')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO todo (title, beskrivning, subject) VALUES ("lär dig pythagoras sats",   "Jag vet, det är svårt att förstå", "Matematik")')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todo.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
 
 Seeder.seed!
